864c45e518f268b72baf1381e0eba9a298c18708
Gayashan Wagachchi <gayashan4lk@gmail.com>
2021-06-14 12:29:59 +0530

    start over

---
+++ src/components/movies.jsx
@@ -0,0 +1,10 @@
+import React, {Component} from "react";
+
+class Movies extends Component{
+    state = {};
+    render() {
+        return <h2>Movies Component</h2>;
+    }
+}
+
+export default Movies;
