30bed5067ab3795ddea66dd89603b4209b337491
Gayashan Wagachchi <gayashan4lk@gmail.com>
2021-06-14 13:16:23 +0530

    add delete button function

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -5,6 +5,12 @@ class Movies extends Component{
     state = {
         movies: getMovies()
     };
+
+    handleDelete = (movie) => {
+        const movies = this.state.movies.filter(m => m._id !== movie._id);
+        this.setState({movies: movies});
+    }
+
     render() {
         return (
                 <table className='table'>
@@ -19,11 +25,12 @@ class Movies extends Component{
                     </thead>
                     <tbody>
                     {this.state.movies.map(movie => (
-                        <tr>
+                        <tr key={movie._id}>
                             <td>{movie.title}</td>
                             <td>{movie.genre.name}</td>
                             <td>{movie.numberInStock}</td>
                             <td>{movie.dailyRentalRate}</td>
+                            <td><button onClick={() => this.handleDelete(movie)} className='btn btn-sm btn-danger'>Delete</button></td>
                         </tr>
                     ))}
                     </tbody>
