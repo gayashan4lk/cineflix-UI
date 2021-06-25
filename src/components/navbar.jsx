41f11c4f9c8296e38de3dcf8337316a3ecd047f0
Gayashan Wagachchi <gayashan4lk@gmail.com>
2021-06-25 11:52:13 +0530

    fixed routing

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,23 +1,24 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import {NavLink} from "react-router-dom";
 
 const NavBar = () => {
     return(
         <nav className="navbar navbar-expand-md navbar-light bg-light mb-5">
-            <Link className="navbar-brand" to="/">Cinetv</Link>
+            <Link className="navbar-brand" to="/"><b>Cinetv</b></Link>
             <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav">
-                    <li className="nav-item active">
-                        <Link className="nav-link" to="/movies">Movies</Link>
+                    <li className="nav-item">
+                        <NavLink className="nav-link" to="/movies"><b>Movies</b></NavLink>
                     </li>
                     <li className="nav-item">
-                        <Link className="nav-link" to="/customers">Customers</Link>
+                        <NavLink className="nav-link" to="/customers"><b>Customers</b></NavLink>
                     </li>
                     <li className="nav-item">
-                        <Link className="nav-link" to="/rentals">Rentals</Link>
+                        <NavLink className="nav-link" to="/rentals"><b>Rentals</b></NavLink>
                     </li>
                     <li className="nav-item">
-                        <Link className="nav-link disabled" to="something">Disabled</Link>
+                        <NavLink className="nav-link disabled" to="disabled"><b>Disabled</b></NavLink>
                     </li>
                 </ul>
             </div>
